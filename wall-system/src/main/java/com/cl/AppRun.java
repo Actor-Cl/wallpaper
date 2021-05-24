3ff14f0e325afcd129ce95f87b49135f80682b53
chenglei <chenglei@hzon.com>
2021-05-24 18:27:46 +0800

    配置问价

---
+++ wall-system/src/main/java/com/cl/AppRun.java
@@ -0,0 +1,32 @@
+package com.cl;
+
+import io.swagger.annotations.Api;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 开启审计功能 -> @EnableJpaAuditing
+ * @author chenglei
+ * @data 2021年05月2021/5/21日 10:55
+ */
+
+@RestController
+@Api(hidden = true)
+@SpringBootApplication
+@EnableTransactionManagement
+//@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class AppRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRun.class,args);
+    }
+
+    @GetMapping("/")
+    public String index(){
+        return "Backend service started successfully";
+    }
+}
